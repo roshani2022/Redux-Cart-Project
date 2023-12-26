c86fded116f4501203f1ce5e653ff83e32a0d275
roshani2022 <rosh.gupta17@gmail.com>
2023-12-26 11:54:12 +0530

    Doing Network calls and andling response the right way

--- src/App.js
+++ src/App.js
@@ -1,15 +1,81 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useEffect, Fragment } from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
-  const showCart = useSelector(state=>state.ui.cartIsVisible)
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+   
+
+  useEffect(() => {
+    const sentCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "sending",
+          message: "sending cart data!",
+        })
+      );
+      const res = await fetch(
+        "https://redux-cart-project-4a610-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("sending cart data failed");
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if(isInitial){
+      isInitial=false;
+      return;
+
+    }
+    sentCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sendind cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart &&< Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
